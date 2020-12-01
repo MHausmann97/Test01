e525adc826d958aa55f406b0d256d13238749239
MHausmann97 <73695590+MHausmann97@users.noreply.github.com>
2020-12-01 18:33:51 +0100

    Initial commit

---
+++ src/test01/Test01.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test01;
+
+/**
+ *
+ * @author marle
+ */
+public class Test01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println(hallo");
+        // TODO code application logic here
+    }
+    
+}
